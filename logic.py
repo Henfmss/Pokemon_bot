67c2c65037e262ec426a2d695842ff08ae58a431
Henfmss <henfm2014@gmail.com>
2025-12-11 18:53:37 +0300

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,118 @@
+import aiohttp  # Eşzamansız HTTP istekleri için bir kütüphane
+import random
+from datetime import datetime, timedelta
+
+class Pokemon:
+    pokemons = {}
+    # Nesne başlatma (kurucu)
+    def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.name = None
+        self.hp = random.randint(200, 400)
+        self.power = random.randint(30, 60)
+        self.last_feed_time = datetime.now()
+
+        if pokemon_trainer not in Pokemon.pokemons:
+            Pokemon.pokemons[pokemon_trainer] = self
+        else:
+            self = Pokemon.pokemons[pokemon_trainer]
+
+    async def get_name(self):
+        # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+            async with session.get(url) as response:  # GET isteği gönderme
+                if response.status == 200:
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    return data['forms'][0]['name']  #  Pokémon adını döndürme
+                else:
+                    return "Pikachu"  # İstek başarısız olursa varsayılan adı döndürür
+
+    async def info(self):
+        # Pokémon hakkında bilgi döndüren bir metot
+        if not self.name:
+            self.name = await self.get_name()  # Henüz yüklenmemişse bir adın geri alınması
+        return f"Pokémonunuzun ismi: {self.name},Pokemonun Gücü:{self.power},Pokemonun Sağlığı:{self.hp}"  # Pokémon adını içeren dizeyi döndürür
+
+    async def show_img(self):
+        # PokeAPI aracılığıyla bir pokémon görüntüsünün URL'sini almak için asenktron metot
+            url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+            async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+                async with session.get(url) as response:  # GET isteği gönderme
+                    if response.status == 200:
+                        data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                        return data['sprites']['front_default']  #  Pokémon adını döndürme
+                    else:
+    
+                        return None  # İstek başarısız olursa varsayılan adı döndürür
+                    
+
+    async def attack(self, enemy):
+        if isinstance(enemy, Wizard):  # Düşmanın Wizard veri tipi olup olmadığının kontrol edilmesi (Sihirbaz sınıfının bir örneği midir?) 
+            sans = random.randint(1, 5) 
+            if sans == 1:
+                return "Sihirbaz Pokémon, savaşta bir kalkan kullanıldı!"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ne saldırdı\n@{enemy.pokemon_trainer}'nin sağlık durumu {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ni yendi!"
+        
+    async def feed(self, feed_interval=20, hp_increase=10):
+        current_time = datetime.now()
+        delta_time = timedelta(seconds=feed_interval)
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Pokémon sağlığı geri yüklenir. Mevcut sağlık: {self.hp}"
+        else:
+            return f"Pokémonunuzu şu zaman besleyebilirsiniz: {self.last_feed_time + delta_time}"
+        
+
+class Wizard(Pokemon):
+    def feed(self, feed_interval=10, hp_increase=10):
+        return super().feed(feed_interval, hp_increase)
+
+class Fighter(Pokemon):
+    async def attack(self, enemy):
+        super_power = random.randint(5, 15)  
+        self.power += super_power
+        sonuc = await super().attack(enemy)  
+        self.power -= super_power
+        return sonuc + f"\nDövüşçü Pokémon süper saldırı kullandı. Eklenen güç: {super_power}"
+    def feed(self, feed_interval=20, hp_increase=20):
+        return super().feed(feed_interval, hp_increase)
+             
+import asyncio
+
+ 
+
+async def main():
+
+    wizard = Wizard("@deniz")
+
+    fighter = Fighter("@atahan")
+
+    
+
+    # await ile coroutine'leri çağırıyoruz
+
+    print(await wizard.info())
+
+    print("#" * 10)
+
+    print(await fighter.info())
+
+    print("#" * 10)
+
+    print(await wizard.attack(fighter))
+
+    print(await fighter.attack(wizard))
+
+ 
+
+# Asenkron main fonksiyonunu çalıştırıyoruz
+
+asyncio.run(main())
